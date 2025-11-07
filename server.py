02ef157b31cfb159ef7015dde06375501ed4a90b
Skobal1090 <skobal1090@gmail.com>
2025-11-07 18:19:45 -0500

    Created basic webpage to send and recieve messages

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sock import Sock
 
 app = Flask(__name__)
 sock = Sock(app)
 
+@app.route('/')
+def home():
+    return render_template('index.html', title='My Flask App', heading='Hello from Flask!')
+    
+
 @sock.route('/reverse')
 def reverse(ws):
     while True:
         text = ws.receive()
+        if text:
+            print(f"Message recieved: {text}")
         ws.send(text[::-1])
 
 if __name__ == "__main__" :
