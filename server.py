77241c86aa024461b24fbf30d0e068d2c48211ab
Skobal1090 <skobal1090@gmail.com>
2025-11-26 00:46:34 -0500

    names now persist between page redirects

--- server.py
+++ server.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sock import Sock
+import json
 
 app = Flask(__name__)
 sock = Sock(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='My Flask App', heading='Hello from Flask!')
+    return render_template('index.html', title='Pictochat', heading='Select a Name')
+
+@app.route('/room')
+def room():
+    return render_template('room.html', title='Pictochat', heading='Room A')
     
 
-@sock.route('/reverse')
+@sock.route('/chatroom')
 def reverse(ws):
     while True:
-        text = ws.receive()
-        if text:
-            print(f"Message recieved: {text}")
-        ws.send(text[::-1])
+        msg = ws.receive()
+        data = json.loads(msg)
+        if msg:
+            print(f"Message recieved!\n From: {data['name']} \n Content: {data['msg']}")
+        ws.send(data)
 
 if __name__ == "__main__" :
     app.run(debug = True, host = "0.0.0.0")
