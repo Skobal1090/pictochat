9367fc82df74d8ea288f14e607cda8f238575242
Skobal1090 <skobal1090@gmail.com>
2025-11-30 01:49:23 -0500

    Added support for multiple connections

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, jsonify
-from flask_sock import Sock
+from flask_socketio import SocketIO, send, emit
 import json
 
 app = Flask(__name__)
-sock = Sock(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
@@ -12,16 +13,14 @@ def home():
 @app.route('/room')
 def room():
     return render_template('room.html', title='Pictochat', heading='Welcome to Pictochat!')
-    
 
-@sock.route('/chatroom')
-def reverse(ws):
-    while True:
-        msg = ws.receive()
-        data = json.loads(msg)
-        if msg:
-            print(f"Message recieved!\n From: {data['name']} \n Content: {data['msg']} \n Time: {data['time']}")
-        ws.send(msg)
+@socketio.on('message')
+def handle_message(msg):
+    data = json.loads(msg)
+    print(f"Message From: {data['name']}: {data['msg']} \n Time: {data['time']}")
+    emit('message', msg, broadcast=True, include_self=True, json=True)
+    print(f"Message returned: {msg}")
+
 
 if __name__ == "__main__" :
-    app.run(debug = True, host = "0.0.0.0")
+    socketio.run(app, debug=True, host="0.0.0.0", allow_unsafe_werkzeug=True)
