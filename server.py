6845d26d128459f69254aa75e7bb1174b99f8ad3
Skobal1090 <skobal1090@gmail.com>
2025-12-01 17:53:58 -0500

    Added join/leave room messages

--- server.py
+++ server.py
@@ -14,12 +14,23 @@ def home():
 def room():
     return render_template('room.html', title='Pictochat', heading='Welcome to Pictochat!')
 
+@socketio.on('joinedRoom')
+def handle_connect(joinedRoomMsg):
+    data = json.loads(joinedRoomMsg)
+    print(f"{data['name']} has joined the room at {data['time']}")
+    emit('joinedRoom', joinedRoomMsg, broadcast = True)
+
+@socketio.on('leftRoom')
+def handle_disconnect(leftRoomMsg):
+    data = json.loads(leftRoomMsg)
+    print(f"{data['name']} has left the room at {data['time']}")
+    emit('leftRoom', leftRoomMsg, broadcast = True)
+
 @socketio.on('message')
 def handle_message(msg):
     data = json.loads(msg)
     print(f"Message From: {data['name']}: {data['msg']} \n Time: {data['time']}")
     emit('message', msg, broadcast=True)
-    print(f"Message returned: {msg}")
 
 
 if __name__ == "__main__" :
