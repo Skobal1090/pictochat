78c25590bf425e45e60d927cbed2c2f1da08aa22
Skobal1090 <skobal1090@gmail.com>
2025-12-12 00:30:41 -0500

    fixed disconnects not working as intended

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, send, emit
+from datetime import datetime
 import json
 
 envVars = []
@@ -11,8 +12,14 @@ except FileNotFoundError:
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = envVars[0]
-socketio = SocketIO(app, max_http_buffer_size=1024*1024*5)
+socketio = SocketIO(app, max_http_buffer_size=1024*1024*5, ping_timeout = 30, ping_interval = 10)
 activeConnections = []
+
+def assignHost(user, data):
+    if user[0] == data['name']:
+            activeConnections.remove(user)
+            if(activeConnections): 
+                emit('hostAssigned', leftRoomMsg, to=activeConnections[0][1])
 
 @app.route('/')
 def home():
@@ -38,7 +45,7 @@ def handle_connect(joinedRoomMsg):
     emit('joinedRoom', joinedRoomMsg, broadcast = True)
 
 @socketio.on('leftRoom')
-def handle_disconnect(leftRoomMsg):
+def handle_leaveRoom(leftRoomMsg):
     data = json.loads(leftRoomMsg)
     print(f"{data['name']} has left the room at {data['time']}")
     for user in activeConnections:
@@ -64,6 +71,20 @@ def handle_canvas(msg):
     data = json.loads(msg)
     print(f"{data['name']} send a drawing at: {data['time']}")
     emit('receiveCanvas', msg, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect(reason):
+    print(reason)
+    for user in activeConnections:
+        if user[1] == request.sid:
+            activeConnections.remove(user)
+            disconnectMsg = {
+                "name" : user[0],
+                "publicKey" : 0
+            }
+            if(activeConnections): 
+                emit('hostAssigned', disconnectMsg, to=activeConnections[0][1])
+            emit('leftRoom', disconnectMsg, broadcast=True)                
 
 if __name__ == "__main__" :
     socketio.run(app, debug=True, host="0.0.0.0", allow_unsafe_werkzeug=True)
